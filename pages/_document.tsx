3d953c8403196bcea69d2b1123ba46291fcbc080
SKfraz04 <Sakib.fraz@gmail.com>
2025-03-04 00:59:15 +0530

    Add page title and update favicon for Commbitz AirIndia

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <title>Commbitz AirIndia</title>
       <body className="antialiased">
         <Main />
         <NextScript />
